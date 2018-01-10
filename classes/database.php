bcd977dc8eb25a3cf0324a2ed67d87d226849a85
Neshkova Bilyana <stc567@ne-149-b63bb4ce.isdads.salford.ac.uk>
2018-01-10 17:30:31 +0000

    added survey page

--- classes/database.php
+++ classes/database.php
@@ -55,6 +55,25 @@ final class database
         return $this->statement->fetchAll();
     }
 
+
+    public function bind($param, $value, $type = null){
+        if (is_null($type)) {
+            switch (true) {
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+            }
+        }
+        $this->statement->bindValue($param, $value, $type);
+    }
     /**
      * Executes the last prepared query and returns a single row of the results
      * @return a single row of the results from the last prepared query
