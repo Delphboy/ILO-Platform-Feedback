c5d6243114425471a3c2561dfb6ef6aa625745ac
Senior Henry <stc765@ne-149-8aacf490.isdads.salford.ac.uk>
2018-01-10 10:51:03 +0000

    started Registration model

---
+++ Models/Register.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Created by Henry Senior
+ * User: stc765
+ * Date: 1/10/18
+ * Time: 10:30 AM
+ */
+class Register
+{
+    /**
+     * Register constructor.
+     * Create a new blank Register object
+     */
+    function __construct()
+    {
+
+    }
+
+    /**
+     * Create a new user, by passing an SQL statement to the PDO
+     * @param $email
+     * @param $password
+     */
+    function addNewUser($email, $password)
+    {
+
+    }
+}
