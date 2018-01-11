5f856ae64a55eeb304dedf258f5306f1d378c8f8
Henry <H.senior@edu.salford.ac.uk>
2018-01-11 14:34:25 +0000

    trying to pull data from registration form

--- register.php
+++ register.php
@@ -2,4 +2,20 @@
 
 $view = new stdClass();
 $view->pageTitle = 'Registration Page';
-require_once('Views/register.phtml');
+
+spl_autoload_register(function($class)
+{
+    include 'Models/' . $class . '.php';
+});
+
+
+if(isset($_POST['submit']))
+{
+    $email = $_POST['email'];
+    $pwd = $_POST['pwd'];
+    $pwd_repeat = $_POST['pwd-repeat'];
+    
+}
+
+require_once('Views/registration.phtml');
+
