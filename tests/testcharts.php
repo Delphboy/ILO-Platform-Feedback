8cb888960847285260474be183fb4825b4d0af75
Steve <stevieag@gmail.com>
2018-01-14 03:43:54 +0000

    Signed-off-by: Steve <stevieag@gmail.com>

--- tests/testcharts.php
+++ tests/testcharts.php
@@ -5,95 +5,191 @@
 		$user = "gr2";
 		$pass = 'gr0up2t357db!';
 
-//		//$dbHandle = new PDO("mysql:host=$host;dbname=$dbName",$user,$pass);
-//		$dbHandle = new PDO("mysql:host=$host;dbname=$dbName",$user,$pass);
-//		$query ="SELECT platform, count(*) FROM review GROUP BY platform";
-//		$sqlQuery = $query; // put your students table name
-//		//echo $sqlQuery;  //helpful for debugging to see what SQL query has been created
-//		$statement = $dbHandle->prepare($sqlQuery); // prepare PDO statement
-//		$statement->execute();   // execute the PDO statement
-//		$results = $statement->fetchAll();
-//		//order the results
-//		echo "<table border='1'>";
-//		//then echo them out:
-//		foreach($results as $key => $result)
-//		{
-//			echo "<tr><td>" . $result[0] ."</td><td>". $result[1]."</td></tr>";
-//		}
-//		echo "</table>";
-//		echo  $results[0];
-//		$dbHandle = null;
-//
-//
-//
+		$dbHandle = new PDO("mysql:host=$host;dbname=$dbName",$user,$pass);
 
-$dbHandle = new PDO("mysql:host=$host;dbname=$dbName",$user,$pass);
-$query ="SELECT platform AS plat, count(platform) AS count FROM review GROUP BY platform ORDER BY count DESC";
-$sqlQuery = $query; // put your students table name
-//echo $sqlQuery;  //helpful for debugging to see what SQL query has been created
-$statement = $dbHandle->prepare($sqlQuery); // prepare PDO statement
-$statement->execute();   // execute the PDO statement
-$results = $statement->fetchAll(PDO::FETCH_ASSOC);//data is here, just draw the chart
+		$query ="SELECT platform AS plat, count(platform) AS count FROM review GROUP BY platform ORDER BY count DESC";
+		$sqlQuery = $query;
+		//echo $sqlQuery;  //helpful for debugging to see what SQL query has been created
+		
+		$statement = $dbHandle->prepare($sqlQuery); // prepare PDO statement
+		$statement->execute();   // execute the PDO statement
+		
+		$results = $statement->fetchAll(PDO::FETCH_ASSOC);//data is here, just draw the chart
 
-//you can access the data in the array like this:
+		//access data in the array
 
-foreach($results as $key => $value){//$value is one row of results
-echo '<some html you="need" for the piechart>'.$value['plat']/*name of the platform*/.'</some closing tag perhaps><another tag>'.$value['count'].'</closed>';
-}
-$dbHandle = null;
+		//foreach($results as $key => $value)
+		//{
+			//$value is one row of results
+			//echo  $value['count'];
+			
+			
+		//}
+		//seeing if i can pull different values from array
+		//$a = $results['AMT'];
+		//list($a,$b)=$results;
+		//echo $a;
+		//echo $b;
+		//var_dump ($results);
+		//$look = $results[0];
+		//echo $look;
+		foreach ($results[1] as $key => $value)
+		{
+				print_r($value->count);
+		}
+		//$value = current ($results[]->plat);
+		//print_r($value);
+		//$value = next ($results);
+		//print_r($value);
+		//$value = next ($results);
+		//print_r($value);
+
+		//print_r (array_values ($results->plat));
+
+		print_r ($results);
+		print_r ($results{3});
+		//echo '<pre>';
+		//print_r($value);
+		//echo '</pre>';
+		
+		
+		$dbHandle = null;
 ?>
-
-
-
-
-
-
-
-
-
 <html>
-<head>
-	<!--Load the AJAX API-->
-	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-	<script type="text/javascript">
+	<head>
+		<!--Load the AJAX API-->
+		<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+			<script type="text/javascript">
 		
-		// Load the Visualization API and the corechart package.
-		google.charts.load('current', {'packages':['corechart']});
-		
-		// Set a callback to run when the Google Visualization API is loaded.
-		google.charts.setOnLoadCallback(drawChart);
-		
-		// Callback that creates and populates a data table,
-		// instantiates the pie chart, passes in the data and
-		// draws it.
+		var $z= 2;
+	//	"<?php ////if (!empty($a))
+	//	   {
+	//	       echo $a;
+	//	   } ?>////" />
+	
+	// Load the Visualization API and the corechart package.
+	google.charts.load('current', {'packages':['corechart']});
+	// Set a callback to run when the Google Visualization API is loaded.
+	google.charts.setOnLoadCallback(drawChart);
+
+	
+		// Creates and populates a data table
 		function drawChart() {
 			
 			// Create the data table.
-			var data = new google.visualization.DataTable();
-			data.addColumn('string', 'Topping');
-			data.addColumn('number', 'Slices');
-			data.addRows([
-				             ['Amazon', 94],
-				             ['Fiverr', 122],
-				             ['AMT', 154],
-				             ['Click Work', 144],
-				             ['Up Work', 287]
-			             ]);
+			var piedata = new google.visualization.DataTable ();
+			piedata.addColumn ('string', 'Platform');
+			piedata.addColumn ('number', 'Popularity');
+			piedata.addRows (
+				[
+					['Amazon', $z],
+					['Fiverr', 8],
+					['AMT', 5],
+					['Click Work', 4],
+					['Up Work', 3]
+				]
+			);
 			
 			// Set chart options
-			var options = {'title':'How Much Pizza I Ate Last Night',
-				'width':400,
-				'height':300};
+			var options =
+				{
+					'title': 'Platforms by popularity',
+					'width': 600,
+					'height': 400
+				};
 			
-			// Instantiate and draw our chart, passing in some options.
-			var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-			chart.draw(data, options);
+			var piechart = new google.visualization.PieChart (document.getElementById ('chart_div'));
+			piechart.draw (piedata, options);
+			
+			//Bar Chart
+			var barchart_options =
+				{
+					title: 'Barchart',
+					width: 600,
+					height: 400,
+					legend: 'none'
+				};
+			
+			var barchart = new google.visualization.BarChart (document.getElementById ('barchart_div'));
+			barchart.draw (piedata, barchart_options);
+			
+			var areadata = google.visualization.arrayToDataTable (
+				[
+					['Platorm', 'Number'],
+					['Amazon', $z],
+					['Fiverr', 8],
+					['AMT', 5],
+					['Click Work', 4],
+					['Up Work', 3]
+				]);
+			
+			//Area Chart
+			var areaChart_options =
+				{
+					title: 'Test Of Area Chart',
+					hAxis: {title: 'Platform', titleTextStyle: {color: '#333'}},
+					vAxis: {minValue: 0}
+				};
+			
+			var areaChart = new google.visualization.AreaChart (document.getElementById ('areaChart_div'));
+			areaChart.draw (areadata, areaChart_options);
 		}
+			
+			google.charts.load ('current', {'packages': ['gauge']});
+			google.charts.setOnLoadCallback (gdrawChart);
+			
+			function gdrawChart () {
+				//gauges
+				
+				var gdata = google.visualization.arrayToDataTable (
+					[
+						['Label', 'Value'],
+						['Amazon', $z],
+						['Fiverr', 8],
+						['AMT', 5],
+						['Click Work', 4],
+						['Up Work', 3]
+					]);
+				
+				var goptions =
+					{
+						width: 600, height: 400,
+//						redFrom: 90, redTo: 100,
+//						yellowFrom: 75, yellowTo: 90,
+						minorTicks: 1
+					};
+				
+				var gchart = new google.visualization.Gauge (document.getElementById ('gchart_div'));
+				
+				gchart.draw (gdata, goptions);
+				
+//				setInterval (function () {gdata.setValue (0, 1, 40 + Math.round (60 * Math.random ()));gchart.draw (gdata, goptions);}, 5000);
+				setInterval (function () {gdata.setValue (0);   gchart.draw (gdata, goptions);});
+				setInterval (function () {gdata.setValue (1);   gchart.draw (gdata, goptions);});
+				setInterval (function () {gdata.setValue (2);   gchart.draw (gdata, goptions);});
+				setInterval (function () {gdata.setValue (3);	gchart.draw (gdata, goptions);});
+				setInterval (function () {gdata.setValue (4);	gchart.draw (gdata, goptions);});
+			}
+		
+
 	</script>
 </head>
 
+
 <body>
-<!--Div that will hold the pie chart-->
-<div id="chart_div"></div>
-</body>
+	<!--chart-->
+	<!--Table and divs that hold the pie charts-->
+		<table class="columns">
+			<tr>
+				<td><div id="chart_div" style="border: 1px solid "></div></td>
+				<td><div id="barchart_div" style="border: 1px solid "></div></td>
+			</tr>
+			<tr>
+				<td><div id="areaChart_div" style="width: 600px; height: 500px;"></div></td>
+				<td><div id="gchart_div" style="width: 600px; height: 500px;"></div></td>
+			</tr>
+		</table>
+	
+	</body>
 </html>
+
