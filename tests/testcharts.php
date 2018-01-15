4a93ece4bbf1fad35f514378d4441d1a8fe7d15c
Neshkova Bilyana <stc567@ne-149-b63bb4ce.isdads.salford.ac.uk>
2018-01-15 14:13:13 +0000

    testchartspage

--- tests/testcharts.php
+++ tests/testcharts.php
@@ -70,7 +70,7 @@
 	// Load the Visualization API and the corechart package.
 	google.charts.load('current', {'packages':['corechart']});
 	// Set a callback to run when the Google Visualization API is loaded.
-	google.charts.setOnLoadCallback(drawChart);
+	godrogle.charts.setOnLoadCallback(awChart);
 
 	
 		// Creates and populates a data table
