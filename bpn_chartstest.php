21d75af601e1e9a3824ea9ccbbd1bf45b934b414
Neshkova Bilyana <stc567@ne-149-b63bb4ce.isdads.salford.ac.uk>
2018-01-16 13:06:30 +0000

    new message

--- bpn_chartstest.php
+++ bpn_chartstest.php
@@ -13,7 +13,7 @@
         // Load the Visualization API and the corechart package.
         google.charts.load('current', {'packages':['corechart']});
         // Set a callback to run when the Google Visualization API is loaded.
-        google.charts.setOnLoadCallback(initialize);
+//        google.charts.setOnLoadCallback();
 
 
             function drawbarchart(){
