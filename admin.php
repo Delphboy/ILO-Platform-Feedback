c0cc679064c5f302b2ea3149ced1b53ac872a754
Henry <H.senior@edu.salford.ac.uk>
2018-01-16 12:50:14 +0000

    session started on admin page

--- admin.php
+++ admin.php
@@ -1,12 +1,11 @@
 <?php
 session_start();
 require_once('Views/template/headerAdmin.phtml');
-var_dump($_SESSION);
 if($_SESSION['isSignedIn'])
 {
     require_once('Views/admin.phtml');
 }
-//else
-//{
-//    header('Location: login.php');
-//}
+else
+{
+    header('Location: login.php');
+}
