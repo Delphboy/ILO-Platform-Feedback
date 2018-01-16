e9d5b1faded8d09721ca62f25d70554ecc12140c
Neshkova Bilyana <stc567@ne-149-b63bb4ce.isdads.salford.ac.uk>
2018-01-16 12:29:07 +0000

    new message

--- bpn_chartstest.php
+++ bpn_chartstest.php
@@ -10,45 +10,39 @@
     print_r($jsonTable);
     ?>
     <script src="js/filters.js"></script>
-<!--    <script type="text/javascript">-->
-<!--        // Load the Visualization API and the corechart package.-->
-<!--        google.charts.load('current', {'packages':['corechart']});-->
-<!--        // Set a callback to run when the Google Visualization API is loaded.-->
-<!--        google.charts.setOnLoadCallback(drawbarchart);-->
-<!---->
-<!---->
-<!--        //first column must be bars, second column must be values-->
-<!--        function drawbarchart() {-->
-<!---->
-<!--            //Create our data table out of JSON data  loaded from server.-->
-<!--            var data = new google.visualization.DataTable();-->
-<!--            var barchart_options =-->
-<!--            {-->
-<!--                title: 'Barchart',-->
-<!--                width: 600,-->
-<!--                height: 400,-->
-<!--                legend: 'none'-->
-<!--            };-->
-<!---->
-<!--            // Instantiate and draw our chart, passing in some options.-->
-<!--            // Do not forget to check your div ID-->
-<!--            var chart = new google.visualization.BarChart(document.getElementById('chart_div'));-->
-<!---->
-<!--            chart.draw(data, barchart_options);-->
-<!--        }-->
-<!--    </script>-->
+    <script type="text/javascript">
+        // Load the Visualization API and the corechart package.
+        google.charts.load('current', {'packages':['corechart']});
+        // Set a callback to run when the Google Visualization API is loaded.
+        //google.charts.setOnLoadCallback(drawbarchart);
+
+
+        //first column must be bars, second column must be values
+        function drawbarchart() {
+
+            //Create our data table out of JSON data  loaded from server.
+            var data = new google.visualization.DataTable(<?php echo $jsonTable; ?>);
+            var barchart_options =
+            {
+                title: 'Barchart',
+                width: 600,
+                height: 400,
+                legend: 'none'
+            };
+
+            // Instantiate and draw our chart, passing in some options.
+            // Do not forget to check your div ID
+            var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+
+            chart.draw(data, barchart_options);
+        }
+    </script>
 </head>
 
 <body>
 <!--this is the div that will hold the pie chart-->
 
 <div id="chart_div">
-    <?php
-    require ('Models/Graph.php');
-    $gr = new Graph();
-    $jsonTable = $gr->fetchData();
-    //print_r($jsonTable);
-    ?>
     <script>drawbarchart(<?php echo $jsonTable; ?>)</script>
 </div>
 </body>
