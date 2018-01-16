5127c62890f4c01e3faab6db9295ab8a803c0df2
Neshkova Bilyana <stc567@ne-149-b63bb4ce.isdads.salford.ac.uk>
2018-01-16 11:42:40 +0000

    new message

--- bpn_chartstest.php
+++ bpn_chartstest.php
@@ -103,33 +103,7 @@ try {
 <head>
     <!--Load the Ajax API-->
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-        // Load the Visualization API and the corechart package.
-        google.charts.load('current', {'packages':['corechart']});
-        // Set a callback to run when the Google Visualization API is loaded.
-        //google.charts.setOnLoadCallback(drawbarchart);
-
-
-        //first column must be bars, second column must be values
-        function drawbarchart() {
-
-            //Create our data table out of JSON data  loaded from server.
-            var data = new google.visualization.DataTable(<?php echo $jsonTable; ?>);
-            var barchart_options =
-            {
-                title: 'Barchart',
-                width: 600,
-                height: 400,
-                legend: 'none'
-            };
-
-            // Instantiate and draw our chart, passing in some options.
-            // Do not forget to check your div ID
-            var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-
-            chart.draw(data, barchart_options);
-        }
-    </script>
+    <script type="text/javascript" src="js/filters.js"></script>
 </head>
 
 <body>
