7e021b84041c581fab64728c5b91fd153f75c698
Senior Henry <stc765@ne-149-b6346c25.isdads.salford.ac.uk>
2018-01-17 14:01:14 +0000

    var dump

--- admin.php
+++ admin.php
@@ -1,35 +1,34 @@
 <?php
-echo 'hello, world';
-//session_start();
-//require_once ('Models/Admin.php');
-//
-//$model = new Admin();
-//
-//echo 'I am a var dump <br/>';
-//var_dump($_POST[]);
-//// Check for the country selection
-//
-//if(isset($_POST['country-selection-admin']))
-//{
-//    //listen for submission
-//    // if it's submitted then:
-//    // grab query -> execute -> return table with values
-//    // display values in DIV
-//    $query = $_POST['country-selection-admin'];
-//    $tableToDisplay = $model->createTable($query);
-//    echo '';
-//    echo $tableToDisplay;
-//}
-//
-//
-//
-//
-//
-//if($_SESSION['isSignedIn'])
-//{
-//    require_once('Views/admin.phtml');
-//}
-//else
-//{
-//    header('Location: login.php');
-//}
+session_start();
+require_once ('Models/Admin.php');
+
+$model = new Admin();
+
+echo 'I am a var dump <br/>';
+var_dump($_POST[]);
+// Check for the country selection
+
+if(isset($_POST['country-selection-admin']))
+{
+    //listen for submission
+    // if it's submitted then:
+    // grab query -> execute -> return table with values
+    // display values in DIV
+    $query = $_POST['country-selection-admin'];
+    $tableToDisplay = $model->createTable($query);
+    echo '';
+    echo $tableToDisplay;
+}
+
+
+
+
+
+if($_SESSION['isSignedIn'])
+{
+    require_once('Views/admin.phtml');
+}
+else
+{
+    header('Location: login.php');
+}
