891aeea2d59638f6d74c070d8e58412fffbd4b5d
Senior Henry <stc765@ne-149-b6346c25.isdads.salford.ac.uk>
2018-01-17 13:57:02 +0000

    var dump

--- admin.php
+++ admin.php
@@ -7,23 +7,24 @@ $model = new Admin();
 if($_SESSION['isSignedIn'])
 {
     require_once('Views/admin.phtml');
+
+    echo 'I am a var dump <br/>';
+    var_dump($_POST[]);
+// Check for the country selection
+
+    if(isset($_POST['country-selection-admin']))
+    {
+        //listen for submission
+        // if it's submitted then:
+        // grab query -> execute -> return table with values
+        // display values in DIV
+        $query = $_POST['country-selection-admin'];
+        $tableToDisplay = $model->createTable($query);
+        echo '';
+        echo $tableToDisplay;
+    }
 }
 else
 {
     header('Location: login.php');
-}
-echo 'I am a var dump <br/>';
-var_dump($_POST['table-submit']);
-// Check for the country selection
-
-if(isset($_POST['table-submit']))
-{
-    //listen for submission
-    // if it's submitted then:
-    // grab query -> execute -> return table with values
-    // display values in DIV
-    $query = $_POST['country-selection-admin'];
-    $tableToDisplay = $model->createTable($query);
-    echo '';
-    echo $tableToDisplay;
 }
