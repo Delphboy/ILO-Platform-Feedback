2f786365ecd3b868e512812d71eb8826eb89aef8
Senior Henry <stc765@ne-149-b6346c25.isdads.salford.ac.uk>
2018-01-17 11:40:14 +0000

    started MVCfication

--- Models/Admin.php
+++ Models/Admin.php
@@ -6,10 +6,39 @@
  * Date: 1/16/18
  * Time: 4:19 PM
  */
+require_once ('Graph.php');
+require_once ('database.php');
 class Admin
 {
+    private $ILODatabase;
+    private $SQLQuery;
+
+    /**
+     * Admin constructor.
+     * Create a connection to the database
+     */
     function __construct()
     {
+        $this->ILODatabase = database::Instance();
+        $this->SQLQuery = NULL;
+    }
+
+    function setQuery($newQuery)
+    {
+        $this->SQLQuery = $newQuery;
+    }
+
+    function executeQuery()
+    {
+        if(($this->SQLQuery != "") || ($this->SQLQuery != NULL))
+        {
+            $this->SQLQuery = $this->ILODatabase->prepare($SQLQuery);
+            $this->SQLQuery->execute();
+        }
+        else
+        {
+
+        }
     }
 
 }
--- admin.php
+++ admin.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 require_once('Views/template/headerAdmin.phtml');
+require_once ('Models/Admin.php');
+
+$model = new Admin();
+
 if($_SESSION['isSignedIn'])
 {
     require_once('Views/admin.phtml');
@@ -8,4 +12,10 @@ if($_SESSION['isSignedIn'])
 else
 {
     header('Location: login.php');
+}
+
+if(isset($_POST['click']))
+{
+    $model->setQuery($_POST['click']);
+    $model->executeQuery();
 }
