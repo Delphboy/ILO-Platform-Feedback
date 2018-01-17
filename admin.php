5cfd8b3373d842058d19e5f20b3cc42973deed61
Senior Henry <stc765@ne-149-b6346c25.isdads.salford.ac.uk>
2018-01-17 13:55:08 +0000

    var dump

--- admin.php
+++ admin.php
@@ -13,15 +13,16 @@ else
     header('Location: login.php');
 }
 echo 'I am a var dump <br/>';
-var_dump($_POST);
+var_dump($_POST['table-submit']);
 // Check for the country selection
+
 if(isset($_POST['table-submit']))
 {
     //listen for submission
     // if it's submitted then:
     // grab query -> execute -> return table with values
     // display values in DIV
-    $query = $_POST['country-selection'];
+    $query = $_POST['country-selection-admin'];
     $tableToDisplay = $model->createTable($query);
     echo '';
     echo $tableToDisplay;
