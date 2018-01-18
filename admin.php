cfa100896907f0f71e56a7a8c31994d0ade0af58
Neshkova Bilyana <stc567@ne-149-b63bb4ce.isdads.salford.ac.uk>
2018-01-18 14:06:28 +0000

    graph fix

--- admin.php
+++ admin.php
@@ -20,22 +20,22 @@ if(isset($_POST['table-submit']))
 
 
 
-if(isset($_POST['GraphSubmit']))
-{
-    $gr = new Graph();
-    switch ($_POST['def-graphs'])
-    {
-        case "PlatformVSWage":
-            $view->grdata = $gr->platform_vs_wage(); break;
-    }
-//    $platform_vs_wage = $gr->platform_vs_wage();
-////print_r($platform_vs_wage);
-//    $wage_per_country = $gr->wage_per_country();
-//    $platform_popularity = $gr->platform_popularity();
-//    $rating_vs_wage = $gr->rating_vs_wage();
-//    $platform_by_rating = $gr->platform_by_rating();
-
-}
+//if(isset($_POST['GraphSubmit']))
+//{
+//    $gr = new Graph();
+//    switch ($_POST['def-graphs'])
+//    {
+//        case "PlatformVSWage":
+//            $view->grdata = $gr->platform_vs_wage(); break;
+//    }
+////    $platform_vs_wage = $gr->platform_vs_wage();
+//////print_r($platform_vs_wage);
+////    $wage_per_country = $gr->wage_per_country();
+////    $platform_popularity = $gr->platform_popularity();
+////    $rating_vs_wage = $gr->rating_vs_wage();
+////    $platform_by_rating = $gr->platform_by_rating();
+//
+//}
 
 
 
