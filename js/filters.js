7feb3d077191dfe1cfcbe08b1c3883f795ae0ea1
Neshkova Bilyana <stc567@ne-149-b63bb4ce.isdads.salford.ac.uk>
2018-01-18 10:47:46 +0000

    graph fix

--- js/filters.js
+++ js/filters.js
@@ -109,35 +109,35 @@ function platform_by_rating(jsontable){
     chart.draw(data, piechart_options);
 }
 
-function drawBasic() {
-
-    google.charts.load('current', {packages: ['corechart', 'bar']});
-
-    var data = google.visualization.arrayToDataTable([
-        ['City', '2010 Population',],
-        ['New York City, NY', 8175000],
-        ['Los Angeles, CA', 3792000],
-        ['Chicago, IL', 2695000],
-        ['Houston, TX', 2099000],
-        ['Philadelphia, PA', 1526000]
-    ]);
-
-    var options = {
-        title: 'Population of Largest U.S. Cities',
-        chartArea: {width: '50%'},
-        hAxis: {
-            title: 'Total Population',
-            minValue: 0
-        },
-        vAxis: {
-            title: 'City'
-        }
-    };
-
-    var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-
-    chart.draw(data, options);
-}
+// function drawBasic() {
+//
+//     google.charts.load('current', {packages: ['corechart', 'bar']});
+//
+//     var data = google.visualization.arrayToDataTable([
+//         ['City', '2010 Population',],
+//         ['New York City, NY', 8175000],
+//         ['Los Angeles, CA', 3792000],
+//         ['Chicago, IL', 2695000],
+//         ['Houston, TX', 2099000],
+//         ['Philadelphia, PA', 1526000]
+//     ]);
+//
+//     var options = {
+//         title: 'Population of Largest U.S. Cities',
+//         chartArea: {width: '50%'},
+//         hAxis: {
+//             title: 'Total Population',
+//             minValue: 0
+//         },
+//         vAxis: {
+//             title: 'City'
+//         }
+//     };
+//
+//     var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+//
+//     chart.draw(data, options);
+// }
 
 //
 // function gender_vs_wage(jsontable){
