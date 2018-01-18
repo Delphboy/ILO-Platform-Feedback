188168297586d2dc8d77218158cba900f7756265
Steve <stevieag@gmail.com>
2018-01-18 14:56:02 +0000

    update

--- index.php
+++ index.php
@@ -32,20 +32,20 @@ if(isset($_POST['submit'])) {
     $dbHandle->execute();
 }
 
-if (isset($_POST['captcha-submit'])) {
+if (isset($_POST['submit'])) {
 	
 	echo "<h1>changed 1</h1>";
 	
-//	$url = 'https://www.google.com/recaptcha/api/siteverify';
-//	$privatekey = "6Lc2O0EUAAAAALHpuswHfe0ruXAOV-jlMs6IKtdm";
-//
-//	$response = file_get_contents ($url . "?secretkey=" . $privatekey . "&response=" . $_POST['g-captcha-response'] . "&remoteip=" . $_SERVER['REMOTE_ADDR']);
-//	$data = json_decode ($response);
-//	echo $url;
-//	echo $privatekey;
-//	echo $response;
-//	echo $data;
-//
+	$url = 'https://www.google.com/recaptcha/api/siteverify';
+	$privatekey = "6LdtaEEUAAAAAIpHKMtXx2Tsf_zTcsHRlYDWGlK-";
+
+	$response = file_get_contents ($url . "?secretkey=" . $privatekey . "&response=" . $_POST['g-captcha-response'] . "&remoteip=" . $_SERVER['REMOTE_ADDR']);
+	$data = json_decode ($response);
+	echo $url;
+	echo $privatekey;
+	echo $response;
+	echo $data;
+
 //	if (isset($data->success) AND $data->success == true) {
 //		//true?
 //		header ('Location: index.php?CaptchaPass=True');
