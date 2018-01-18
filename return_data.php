35ab7d5fa2efb403d6556084869171ce33454da6
Neshkova Bilyana <stc567@ne-149-b63bb4ce.isdads.salford.ac.uk>
2018-01-18 16:23:12 +0000

    graph fix

--- return_data.php
+++ return_data.php
@@ -5,12 +5,12 @@ $gr = new Graph();
 
 $grdata = null;
 
-//if (isset($_POST['GraphSubmit'])){
+
     switch ($_POST['def_graphs']) {
         case "PlatformVSWage":
             $grdata = $gr->platform_vs_wage();
+            echo "<script type=\"text/javascript\">platform_vs_wage('$grdata');</script>";
             break;
-//    }
+
 }
 //echo "<p>This is text</p>";
-echo "<script type=\"text/javascript\">platform_vs_wage('$grdata');</script>";
