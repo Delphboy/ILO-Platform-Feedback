36bbcfc34ed600b54668f01502d53ff16eb5fee7
Steve <stevieag@gmail.com>
2018-01-18 17:47:36 +0000

    checker , we think it works.... ;-)

--- index.php
+++ index.php
@@ -13,47 +13,41 @@ if(isset($_POST['submit'])){
 	
 	if(isset($data->success) AND $data->success==true){
 		
-	echo "<h1>PASS</h1>";
+		$platform = $_POST['platform'];
+		$timelooking = $_POST['timelooking'];
+		$timeworking = $_POST['timeworking'];
+		$currencySign = $_POST['CurrencySign'];
+		$wage = $_POST['wage'];
+		$gender = $_POST['gender'];
+		$age = $_POST['age'];
+		$country = $_POST['country'];
+		$rating = $_POST['rating'];
+		
+		$dbHandle = database::Instance();
+		$query = "INSERT INTO review(platform, wage, currency, hours_spent_working, hours_spent_looking, gender, age, rating, country) VALUES(:platform, :wage, :CurrencySign, :timeworking, :timelooking, :gender, :age, :rating, :country);";
+		$dbHandle->query($query);
+		
+		$dbHandle->bind(':platform',$platform);
+		$dbHandle->bind(':wage', $wage);
+		$dbHandle->bind(':CurrencySign',$currencySign);
+		$dbHandle->bind(':timeworking',$timeworking);
+		$dbHandle->bind(':timelooking',$timelooking);
+		$dbHandle->bind(':gender',$gender);
+		$dbHandle->bind(':age',$age);
+		$dbHandle->bind(':rating',$rating);
+		$dbHandle->bind(':country',$country);
+		//var_dump ($dbHandle);
+		$dbHandle->execute();
 		
 	}else{
 	
-	echo "<h1>FAIL</h1>";
+	echo '<script language = "javascript">';
+	echo 'alert("FAIL")';
+	echo '</script>';
 	
 	}
 	
 }
-
-//If the data has been submitted
-if(isset($_POST['submit'])) {
-//pull data from database [ IT WORKS! LEAVE IT ALONE!!!! thank you ]
-    $platform = $_POST['platform'];
-    $timelooking = $_POST['timelooking'];
-    $timeworking = $_POST['timeworking'];
-    $currencySign = $_POST['CurrencySign'];
-    $wage = $_POST['wage'];
-    $gender = $_POST['gender'];
-    $age = $_POST['age'];
-    $country = $_POST['country'];
-    $rating = $_POST['rating'];
-
-    $dbHandle = database::Instance();
-    $query = "INSERT INTO review(platform, wage, currency, hours_spent_working, hours_spent_looking, gender, age, rating, country) VALUES(:platform, :wage, :CurrencySign, :timeworking, :timelooking, :gender, :age, :rating, :country);";
-    $dbHandle->query($query);
-
-    $dbHandle->bind(':platform',$platform);
-    $dbHandle->bind(':wage', $wage);
-    $dbHandle->bind(':CurrencySign',$currencySign);
-    $dbHandle->bind(':timeworking',$timeworking);
-    $dbHandle->bind(':timelooking',$timelooking);
-    $dbHandle->bind(':gender',$gender);
-    $dbHandle->bind(':age',$age);
-    $dbHandle->bind(':rating',$rating);
-    $dbHandle->bind(':country',$country);
-    //var_dump ($dbHandle);
-    $dbHandle->execute();
-}
-
-
 
 require_once('Views/index.phtml');
 
