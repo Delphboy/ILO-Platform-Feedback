c74afe8c5d76d0337d80341e4201d00e8ff43769
Henry <H.senior@edu.salford.ac.uk>
2018-01-18 15:32:43 +0000

    working on javascript filters

--- js/filters.js
+++ js/filters.js
@@ -15,7 +15,7 @@ function searchByPlatform() {
     var input, filter, table, tr, td, i;
     input = document.getElementById("platformInput");
     filter = input.value.toUpperCase();
-    table = document.getElementById("myTable");
+    table = document.getElementById("tableToDisplay");
     tr = table.getElementsByTagName("tr");
 
     // Loop through all table rows, and hide those who don't match the search query
