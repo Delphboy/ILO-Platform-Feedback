3aa6409c3144a0171f317dbe9a919b9d1b4a5368
Neshkova Bilyana <stc567@ne-149-b63bb4ce.isdads.salford.ac.uk>
2018-01-19 13:00:20 +0000

    graph refactor

--- js/filters.js
+++ js/filters.js
@@ -95,39 +95,6 @@ function drawPieChart(jsontable) {
     })
 }
 
-function platform_popularity(jsontable) {
-    google.charts.load('43', {'packages': ['corechart']});
-    var data = new google.visualization.DataTable(jsontable);
-    var piechart_options =
-    {
-        title: 'Average Platform Popularity',
-        width: 600,
-        height: 600,
-        legend: 'none'
-    };
-    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-
-    chart.draw(data, piechart_options);
-}
-
-function platform_vs_wage(jsontable){
-    google.charts.load('43', {'packages': ['corechart']});
-    google.charts.setOnLoadCallback(function () {
-    var data = new google.visualization.DataTable(jsontable);
-    var barchart_options =
-        {
-            title: 'Platform vs Average Wage',
-            width: 600,
-            height: 600,
-            legend: 'none'
-        };
-
-    // Instantiate and draw our chart, passing in some options.
-    // Do not forget to check your div ID
-    var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-    chart.draw(data, barchart_options);
-    })
-}
 
 function wage_per_country(jsontable) {
     google.charts.load('43', {'packages': ['corechart']});
@@ -163,65 +130,3 @@ function rating_vs_wage(jsontable) {
 
     chart.draw(data, piechart_options);
 }
-
-function platform_by_rating(jsontable) {
-    google.charts.load('43', {'packages': ['corechart']});
-    var data = new google.visualization.DataTable(jsontable);
-    var piechart_options =
-    {
-        title: 'Platform by Average Rating',
-        width: 600,
-        height: 600,
-        legend: 'none'
-    };
-    var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-
-    chart.draw(data, piechart_options);
-}
-
-function drawBasic() {
-    google.charts.load('current', {packages: ['corechart', 'bar']});
-    google.charts.setOnLoadCallback(function () {
-        var data = google.visualization.arrayToDataTable([
-            ['City', '2010 Population',],
-            ['New York City, NY', 8175000],
-            ['Los Angeles, CA', 3792000],
-            ['Chicago, IL', 2695000],
-            ['Houston, TX', 2099000],
-            ['Philadelphia, PA', 1526000]
-        ]);
-
-        var options = {
-            title: 'Population of Largest U.S. Cities',
-            chartArea: {width: '50%'},
-            hAxis: {
-                title: 'Total Population',
-                minValue: 0
-            },
-            vAxis: {
-                title: 'City'
-            }
-        };
-
-        var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-
-        chart.draw(data, options);
-    })
-
-
-}
-
-//
-// function gender_vs_wage(jsontable){
-//     var data = new google.visualization.DataTable(jsontable);
-//     var piechart_options =
-//     {
-//         title: 'Barchart',
-//         width: 600,
-//         height: 600,
-//         legend: 'none'
-//     };
-//     var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-//
-//     chart.draw(data, piechart_options);
-// }
--- return_data.php
+++ return_data.php
@@ -23,30 +23,6 @@ if (isset($_POST['var1']) && isset($_POST['var2']))
 
     }
 
-//    switch ($_POST['def_graphs']) {
-//        case "PlatformVSWage":
-//            $grdata = $gr->platform_vs_wage();
-//            echo "<script type=\"text/javascript\">platform_vs_wage('$grdata');</script>";
-//            break;
-//        case "WagePerCountry" :
-//            $grdata = $gr->wage_per_country();
-//            echo "<script type=\"text/javascript\">wage_per_country('$grdata');</script>";
-//            break;
-//        case "PlatformPopularity" :
-//            $grdata = $gr->platform_popularity();
-//            echo "<script type=\"text/javascript\">platform_popularity('$grdata');</script>";
-//            break;
-//        case "RatingVSWage" :
-//            $grdata = $gr->rating_vs_wage();
-//            echo "<script type=\"text/javascript\">rating_vs_wage('$grdata');</script>";
-//            break;
-//        case "PlatformByRating" :
-//            $grdata = $gr->platform_by_rating();
-//            echo "<script type=\"text/javascript\">platform_by_rating('$grdata');</script>";
-//            break;
-//    }
-
-
 
 if (isset($_POST['country-selection-admin'])){
     $query = $_POST['country-selection-admin'];
