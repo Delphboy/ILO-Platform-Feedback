10d543638627b3279b919ae7c801a667fb29dd15
Neshkova Bilyana <stc567@ne-149-b63bb4ce.isdads.salford.ac.uk>
2018-01-19 16:35:28 +0000

    graph refactor

--- admin.php
+++ admin.php
@@ -8,31 +8,40 @@ $view = new stdClass();
 $model = new Admin();
 $gr = new Graph();
 
-// Check for the country selection
-//if (isset($_POST['table-submit'])) {
-//    $query = $_POST['country-selection-admin'];
-//    $tableToDisplay = $model->createTable($query);
-//
-//    if ($tableToDisplay != "") {
-//        $view->table = $tableToDisplay;
-//    }
-//}
+$grdata = null;
 
-//if (isset($_POST['GraphSubmit'])){
-//    switch ($_POST['def_graphs']) {
-//        case "PlatformVSWage":
-//            $view->grdata = $gr->platform_vs_wage();
-//            break;
-//    }
-//    echo $view->grdata;
-//}
-//    $platform_vs_wage = $gr->platform_vs_wage();
-////print_r($platform_vs_wage);
-//    $wage_per_country = $gr->wage_per_country();
-//    $platform_popularity = $gr->platform_popularity();
-//    $rating_vs_wage = $gr->rating_vs_wage();
-//    $platform_by_rating = $gr->platform_by_rating();
+if (isset($_POST['var1']) && isset($_POST['var2'])) {
+    //$query = $_POST['def_graphs'];
+    if (isset($_POST['var3']) && $_POST['var3'] != "") {
+        $query = 'SELECT ' . $_POST['var1'] . ', ' . $_POST['var2'] . ', ' . $_POST['var3'] . ' FROM review ' . $_POST['group'];
+        $grdata = $gr->getJson3fields($query, $_POST['var1'], $_POST['var2'], $_POST['var3']);
+    } else {
+        $query = 'SELECT ' . $_POST['var1'] . ', ' . $_POST['var2'] . ' FROM review ' . $_POST['group'];
+        $grdata = $gr->getJson2fields($query, $_POST['var1'], $_POST['var2']);
+    }
 
+    echo "<h3>$query</h3>";
+
+
+    echo "<p>$grdata</p>";
+    if ($_POST['chart'] == 'barchart') {
+        echo "<script type=\"text/javascript\">drawBarChart('$grdata');</script>";
+    } elseif ($_POST['chart'] == 'piechart') {
+        echo "<script type=\"text/javascript\">drawPieChart('$grdata');</script>";
+    } elseif ($_POST['chart'] == 'areachart') {
+        echo "<script type=\"text/javascript\">drawAreaChart('$grdata');</script>";
+    }
+}
+
+if (isset($_POST['country-selection-admin'])){
+    $query = $_POST['country-selection-admin'];
+    $tableToDisplay = $model->createTable($query);
+
+    if ($tableToDisplay != "") {
+        $table = $tableToDisplay;
+        echo "$table";
+    }
+}
 
 session_start();
 //if ($_SESSION['isSignedIn']) {
