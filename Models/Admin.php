70dddf0d306568ec0051be91c88e4aa347786de4
Henry Senior <hps1945@live.co.uk>
2018-01-19 17:43:31 +0000

    flag extreme data

--- Models/Admin.php
+++ Models/Admin.php
@@ -12,6 +12,21 @@ class Admin
 {
     private $ILODatabase;
 
+    private $AMTAvg;
+    private $ClickWorkerAvg;
+    private $CraigslistAvg;
+    private $CrowdSourceAvg;
+    private $ElanceAvg;
+    private $FiverrAvg;
+    private $FreelancerAvg;
+    private $GetACoderAvg;
+    private $GuruAvg;
+    private $PeopleperhourAvg;
+    private $Project4hireAvg;
+    private $ToptalAvg;
+    private $UpworkAvg;
+    private $iFreelanceAvg;
+
     /**
      * Admin constructor.
      * Create a connection to the database
@@ -19,6 +34,21 @@ class Admin
     function __construct()
     {
         $this->ILODatabase = database::Instance();
+
+        $this->AMTAvg = $this->calculatePlatformAverage('AMT');
+        $this->ClickWorkerAvg = $this->calculatePlatformAverage('ClickWorker');
+        $this->CraigslistAvg = $this->calculatePlatformAverage('Craigslist');
+        $this->CrowdSourceAvg = $this->calculatePlatformAverage('CrowdSource');
+        $this->ElanceAvg = $this->calculatePlatformAverage('Elance');
+        $this->FiverrAvg = $this->calculatePlatformAverage('Fiverr');
+        $this->FreelancerAvg = $this->calculatePlatformAverage('Freelancer');
+        $this->GetACoderAvg = $this->calculatePlatformAverage('GetACoder');
+        $this->GuruAvg = $this->calculatePlatformAverage('Guru');
+        $this->PeopleperhourAvg = $this->calculatePlatformAverage('Peopleperhour');
+        $this->Project4hireAvg = $this->calculatePlatformAverage('Project4hire');
+        $this->ToptalAvg = $this->calculatePlatformAverage('Toptal');
+        $this->UpworkAvg = $this->calculatePlatformAverage('Upwork');
+        $this->iFreelanceAvg = $this->calculatePlatformAverage('iFreelance');
     }
 
     function createTable($statement)
@@ -38,8 +68,12 @@ class Admin
 //            //echo "<br>";
 //            $colcount = count($row);
 
+            $flag = $this->isExtreme($data[$rowno]);
 
-            $output = $output . '<tr>';
+            if($flag)
+                $output = $output . '<tr style="color: red">';
+            else
+                $output = $output . '<tr>';
 
             //hardcoded for the moment as $colcount doubles the size of the array
             for($colno = 0; $colno < ($colcount/2); $colno++)
@@ -53,4 +87,104 @@ class Admin
         return $output;
     }
 
+    /**
+     * Search for all the tuples belonging to a specified platform
+     * Then calculate and return the average
+     * @param $platform
+     * @return average of the platform
+     */
+    function calculatePlatformAverage($platform)
+    {
+        $dbConnection = database::Instance();
+        $query = "SELECT platform, (AVG(wage) + AVG
+(hours_spent_looking) + AVG(hours_spent_working) + AVG(rating_pay)+
+                  AVG(rating_conditions) + AVG(rating_description) + AVG(rating)) / (SELECT(COUNT(platform)))
+FROM gr2.review
+WHERE platform=:plat;";;
+        $dbConnection->query($query);
+        $dbConnection->bind(':plat', $platform);
+        $dbConnection->execute();
+        $row = $dbConnection->resultSet();
+        $average = $row[0][1];
+        return $average;
+    }
+
+    /**
+     * Calculate the average of the current tuple
+     * @param $tuple
+     * @return array
+     */
+    function calculateTupleAverage($tuple)
+    {
+        $average = ($tuple[2] + $tuple[4] + $tuple[5] + $tuple[8] + $tuple[10]
+            + $tuple[11] + $tuple[12]) / 7;
+        return $average;
+    }
+
+    /**
+     * Calculate whether or not a tuple is in the extremes of the data for it's
+     * respective platform
+     * @param $tuple
+     * @return bool
+     */
+    function isExtreme($tuple)
+    {
+        $platform = $tuple[1];
+        $tupleAvg = $this->calculateTupleAverage($tuple);
+
+        switch($platform)
+        {
+            case 'AMT':
+                $platformAvg = $this->AMTAvg;
+                break;
+            case 'ClickWorker':
+                $platformAvg = $this->ClickWorkerAvg;
+                break;
+            case 'Craigslist':
+                $platformAvg = $this->CraigslistAvg;
+                break;
+            case 'CrowdSource':
+                $platformAvg = $this->CrowdSourceAvg;
+                break;
+            case 'Elance':
+                $platformAvg = $this->ElanceAvg;
+                break;
+            case 'Fiverr':
+                $platformAvg = $this->FiverrAvg;
+                break;
+            case 'Freelancer':
+                $platformAvg = $this->FreelancerAvg;
+                break;
+            case 'GetACoder':
+                $platformAvg = $this->GetACoderAvg;
+                break;
+            case 'Guru':
+                $platformAvg = $this->GuruAvg;
+                break;
+            case 'Peopleperhour':
+                $platformAvg = $this->PeopleperhourAvg;
+                break;
+            case 'Project4hire':
+                $platformAvg = $this->Project4hireAvg;
+                break;
+            case 'Toptal':
+                $platformAvg = $this->ToptalAvg;
+                break;
+            case 'Upwork':
+                $platformAvg = $this->UpworkAvg;
+                break;
+            case 'iFreelance':
+                $platformAvg = $this->iFreelanceAvg;
+                break;
+            default:
+                $platformAvg = $this->AMTAvg;
+                break;
+        }
+
+        $avgWidth = $platformAvg * 2;
+        $percentile = ($avgWidth / 100) * 5;
+
+        return ($tupleAvg < ($percentile)
+            || $tupleAvg > ($avgWidth - $percentile));
+    }
 }
