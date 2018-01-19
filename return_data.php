048e98fec6662d7451699e382867438547f08821
Neshkova Bilyana <stc567@ne-149-b63bb4ce.isdads.salford.ac.uk>
2018-01-19 16:34:27 +0000

    graph refactor

--- return_data.php
+++ return_data.php
@@ -8,13 +8,12 @@ $model = new Admin();
 
 $grdata = null;
 
-if (isset($_POST['var1']) && isset($_POST['var2']))
+if (isset($_POST['var1']) && isset($_POST['var2'])) {
     //$query = $_POST['def_graphs'];
-    if (isset($_POST['var3']) && $_POST['var3'] != ""){
-        $query = 'SELECT ' . $_POST['var1'] . ', ' . $_POST['var2']. ', ' . $_POST['var3'] . ' FROM review ' . $_POST['group'];
+    if (isset($_POST['var3']) && $_POST['var3'] != "") {
+        $query = 'SELECT ' . $_POST['var1'] . ', ' . $_POST['var2'] . ', ' . $_POST['var3'] . ' FROM review ' . $_POST['group'];
         $grdata = $gr->getJson3fields($query, $_POST['var1'], $_POST['var2'], $_POST['var3']);
-    }
-    else {
+    } else {
         $query = 'SELECT ' . $_POST['var1'] . ', ' . $_POST['var2'] . ' FROM review ' . $_POST['group'];
         $grdata = $gr->getJson2fields($query, $_POST['var1'], $_POST['var2']);
     }
@@ -23,16 +22,14 @@ if (isset($_POST['var1']) && isset($_POST['var2']))
 
 
     echo "<p>$grdata</p>";
-    if ($_POST['chart'] == 'barchart'){
+    if ($_POST['chart'] == 'barchart') {
         echo "<script type=\"text/javascript\">drawBarChart('$grdata');</script>";
-    }
-    elseif ($_POST['chart'] == 'piechart'){
+    } elseif ($_POST['chart'] == 'piechart') {
         echo "<script type=\"text/javascript\">drawPieChart('$grdata');</script>";
-    }
-    elseif ($_POST['chart'] == 'areachart'){
+    } elseif ($_POST['chart'] == 'areachart') {
         echo "<script type=\"text/javascript\">drawAreaChart('$grdata');</script>";
     }
-
+}
 
 if (isset($_POST['country-selection-admin'])){
     $query = $_POST['country-selection-admin'];
