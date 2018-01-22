dd4801558b47e1f680ff1a817e1f9336018eb8e4
Henry Senior <hps1945@live.co.uk>
2018-01-22 14:46:40 +0000

    Added bilyanas fix

--- js/filters.js
+++ js/filters.js
@@ -63,174 +63,88 @@ function searchByWage()
 /**
  * CHART FILTERS
  */
-
-
-function initialize() {
-    var opts = {sendMethod: 'auto'};
-    // Replace the data source URL on next line with your data source URL.
-    var query = new google.visualization.Query("csv?url=/data.csv", opts);
-    // Optional request to return only column C and the sum of column B, grouped by C members.
-    query.setQuery('select gender, rating');
-
-    // Send the query with a callback function.
-    query.send(handleQueryResponse);
-}
-
-function handleQueryResponse(response) {
-    if (response.isError()) {
-        alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-        return;
-    }
-    var data = response.getDataTable();
-    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-    chart.draw(data, {width: 400, height: 240, is3D: true});
-}
-
-// function makeBarChart(CSVTable, column1, column2)
-// {
-//     google.charts.load('43', {'packages': ['corechart']});
-//     google.charts.setOnLoadCallback(function () {
-//         var data = new google.visualization.DataTable(CSVTable);
-//         var barchart_options =
-//             {
-//                 title: 'Bar Chart',
-//                 width: 600,
-//                 height: 600,
-//                 legend: 'none'
-//             };
-//
-//         // Instantiate and draw our chart, passing in some options.
-//         // Do not forget to check your div ID
-//         var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-//         chart.draw(data, barchart_options);
-//     })
-// }
-//
-function platform_vs_wage(jsontable){
+function drawBarChart(jsontable) {
     google.charts.load('43', {'packages': ['corechart']});
     google.charts.setOnLoadCallback(function () {
-    var data = new google.visualization.DataTable(jsontable);
-    var barchart_options =
-        {
-            title: 'Platform vs Average Wage',
-            width: 600,
-            height: 600,
-            legend: 'none'
-        };
-
-    // Instantiate and draw our chart, passing in some options.
-    // Do not forget to check your div ID
-    var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-    chart.draw(data, barchart_options);
+        var data = new google.visualization.DataTable(jsontable);
+        var rowcount = data.getNumberOfRows();
+        var barchart_options;
+        if (rowcount > 15){
+            barchart_options =
+                {
+                    title: '',
+                    width: 1500,
+                    chartArea: { left: 30, top: 5, width: '80%', height: '80%'},
+                    height: 600,
+                };
+        }
+        else{
+            barchart_options =
+                {
+                    title: '',
+                    width: 900,
+                    chartArea: {left: 30, top:5, width: '80%', height: '80%'},
+                    height: 600,
+                };
+        }
+        var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+        chart.draw(data, barchart_options);
     })
 }
-//
-// function wage_per_country(jsontable) {
-//     google.charts.load('43', {'packages': ['corechart']});
-//     google.charts.setOnLoadCallback(function () {
-//         var data = new google.visualization.DataTable(jsontable);
-//         var barchart_options =
-//         {
-//             title: 'Average Wage per Country',
-//             width: 600,
-//             height: 600,
-//             legend: 'none'
-//         };
-//         var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-//
-//         chart.draw(data, barchart_options);
-//     })
-// }
-//
-// function platform_popularity(jsontable) {
-//     google.charts.load('43', {'packages': ['corechart']});
-//     var data = new google.visualization.DataTable(jsontable);
-//     var piechart_options =
-//     {
-//         title: 'Average Platform Popularity',
-//         width: 600,
-//         height: 600,
-//         legend: 'none'
-//     };
-//     var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-//
-//     chart.draw(data, piechart_options);
-// }
-// function rating_vs_wage(jsontable) {
-//     google.charts.load('43', {'packages': ['corechart']});
-//     var data = new google.visualization.DataTable(jsontable);
-//     var piechart_options =
-//     {
-//         title: 'Hours Looking vs Hours Working',
-//         hAxis: {title: 'hours looking', minValue: 0, maxValue: 100},
-//         vAxis: {title: 'hours working', minValue: 0, maxValue: 100},
-//         width: 600,
-//         height: 600,
-//         legend: 'none'
-//     };
-//     var chart = new google.visualization.ScatterChart(document.getElementById('chart_div'));
-//
-//     chart.draw(data, piechart_options);
-// }
-//
-// function platform_by_rating(jsontable) {
-//     google.charts.load('43', {'packages': ['corechart']});
-//     var data = new google.visualization.DataTable(jsontable);
-//     var piechart_options =
-//     {
-//         title: 'Platform by Average Rating',
-//         width: 600,
-//         height: 600,
-//         legend: 'none'
-//     };
-//     var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-//
-//     chart.draw(data, piechart_options);
-// }
-//
-// function drawBasic() {
-//     google.charts.load('current', {packages: ['corechart', 'bar']});
-//     google.charts.setOnLoadCallback(function () {
-//         var data = google.visualization.arrayToDataTable([
-//             ['City', '2010 Population',],
-//             ['New York City, NY', 8175000],
-//             ['Los Angeles, CA', 3792000],
-//             ['Chicago, IL', 2695000],
-//             ['Houston, TX', 2099000],
-//             ['Philadelphia, PA', 1526000]
-//         ]);
-//
-//         var options = {
-//             title: 'Population of Largest U.S. Cities',
-//             chartArea: {width: '50%'},
-//             hAxis: {
-//                 title: 'Total Population',
-//                 minValue: 0
-//             },
-//             vAxis: {
-//                 title: 'City'
-//             }
-//         };
-//
-//         var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-//
-//         chart.draw(data, options);
-//     })
-//
-//
-// }
 
-//
-// function gender_vs_wage(jsontable){
-//     var data = new google.visualization.DataTable(jsontable);
-//     var piechart_options =
-//     {
-//         title: 'Barchart',
-//         width: 600,
-//         height: 600,
-//         legend: 'none'
-//     };
-//     var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-//
-//     chart.draw(data, piechart_options);
-// }
+function drawPieChart(jsontable) {
+    google.charts.load('43', {'packages': ['corechart']});
+    google.charts.setOnLoadCallback(function () {
+        var data = new google.visualization.DataTable(jsontable);
+        var rowcount = data.getNumberOfRows();
+        var barchart_options;
+        if (rowcount > 15){
+            barchart_options =
+                {
+                    title: '',
+                    width: 1500,
+                    chartArea: { left: 30, top: 5, width: '80%', height: '80%'},
+                    height: 600,
+                };
+        }
+        else{
+            barchart_options =
+                {
+                    title: '',
+                    width: 900,
+                    chartArea: {left: 30, top:5, width: '80%', height: '80%'},
+                    height: 600,
+                };
+        }
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, barchart_options);
+    })
+}
+function drawAreaChart(jsontable) {
+    google.charts.load('43', {'packages': ['corechart']});
+    google.charts.setOnLoadCallback(function () {
+        var data = new google.visualization.DataTable(jsontable);
+        var rowcount = data.getNumberOfRows();
+        var barchart_options;
+        if (rowcount > 15){
+            barchart_options =
+                {
+                    title: '',
+                    width: 1500,
+                    chartArea: { left: 30, top: 5, width: '80%', height: '80%'},
+                    height: 600,
+                };
+        }
+        else{
+            barchart_options =
+                {
+                    title: '',
+                    width: 900,
+                    chartArea: {left: 30, top:5, width: '80%', height: '80%'},
+                    height: 600,
+                };
+        }
+        var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
+        chart.draw(data, barchart_options);
+    })
+}
--- return_data.php
+++ return_data.php
@@ -1,30 +1,33 @@
 <?php
 require_once('Models/Graph.php');
-
+require_once('Models/Admin.php');
 $gr = new Graph();
-
+$model = new Admin();
 $grdata = null;
-
-
-switch ($_POST['def_graphs']) {
-    case "PlatformVSWage":
-        $grdata = $gr->platform_vs_wage();
-        echo "<script type=\"text/javascript\">platform_vs_wage('$grdata');</script>";
-        break;
-    case "WagePerCountry" :
-        $grdata = $gr->wage_per_country();
-        echo "<script type=\"text/javascript\">wage_per_country('$grdata');</script>";
-        break;
-    case "PlatformPopularity" :
-        $grdata = $gr->platform_popularity();
-        echo "<script type=\"text/javascript\">platform_popularity('$grdata');</script>";
-        break;
-    case "RatingVSWage" :
-        $grdata = $gr->rating_vs_wage();
-        echo "<script type=\"text/javascript\">rating_vs_wage('$grdata');</script>";
-        break;
-    case "PlatformByRating" :
-        $grdata = $gr->platform_by_rating();
-        echo "<script type=\"text/javascript\">platform_by_rating('$grdata');</script>";
-        break;
+if (isset($_POST['var1']) && isset($_POST['var2'])) {
+    //$query = $_POST['def_graphs'];
+    if (isset($_POST['var3']) && $_POST['var3'] != "") {
+        $query = 'SELECT ' . $_POST['var1'] . ', ' . $_POST['var2'] . ', ' . $_POST['var3'] . ' FROM review ' . $_POST['group'];
+        $grdata = $gr->getJson3fields($query, $_POST['var1'], $_POST['var2'], $_POST['var3']);
+    } else {
+        $query = 'SELECT ' . $_POST['var1'] . ', ' . $_POST['var2'] . ' FROM review ' . $_POST['group'];
+        $grdata = $gr->getJson2fields($query, $_POST['var1'], $_POST['var2']);
+    }
+    echo "<h3>$query</h3>";
+    echo "<p>$grdata</p>";
+    if ($_POST['chart'] == 'barchart') {
+        echo "<script type=\"text/javascript\">drawBarChart('$grdata');</script>";
+    } elseif ($_POST['chart'] == 'piechart') {
+        echo "<script type=\"text/javascript\">drawPieChart('$grdata');</script>";
+    } elseif ($_POST['chart'] == 'areachart') {
+        echo "<script type=\"text/javascript\">drawAreaChart('$grdata');</script>";
+    }
+}
+if (isset($_POST['country-selection-admin'])){
+    $query = $_POST['country-selection-admin'];
+    $tableToDisplay = $model->createTable($query);
+    if ($tableToDisplay != "") {
+        $table = $tableToDisplay;
+        echo "$table";
+    }
 }
